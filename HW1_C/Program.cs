1d1fe1e2636094da6a3b8cbeb9aca74a1bb09148
Vladimir <Steamfor97@mail.ru>
2021-04-27 02:36:34 +0300

    wtf

---
+++ HW1_C/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HomeWork1_C
+{
+    class HW1
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = DateTime.Now;
+            Console.WriteLine("Введите ваше имя");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Привет,{name},сегодня {date}");
+            Console.ReadLine();
+        }
+    }
+}
